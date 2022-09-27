9947e675a2d1256e1f7f863087e1947bae136b6a
shifa7699 <111556272+shifa7699@users.noreply.github.com>
2022-09-27 13:37:04 +0530

    Add files via upload

---
+++ d_construct.java
@@ -0,0 +1,16 @@
+class Defaultcon
+{
+int age;
+String name;
+void show()
+{
+System.out.println(age+" "+name);
+}
+public static void main(String args[])
+{
+Defaultcon d=new Defaultcon();
+//Defaultcon d1=new Defaultcon();
+d.show();
+//d1.show();
+}
+}
