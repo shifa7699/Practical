564df7bf2f540a6e281e46a9bb451a5289ac922c
shifa7699 <111556272+shifa7699@users.noreply.github.com>
2022-09-27 12:59:24 +0530

    Add files via upload

---
+++ paracons.java
@@ -0,0 +1,23 @@
+class parameterizedcons
+{
+int age;
+String name;
+//Parametrized constructor where we have to pass the parameter
+parametrizedcons(int a,String n)
+{
+age=a;
+name=n;
+}
+void show()
+{
+System.out.println(age+" "+name);
+}
+public static void main(String[] args)
+{
+parameterizedcons d=new parametrizedcons("50","Karan");
+parameterizedcons d1=new parametrizedcons("25","Rohit");
+d.show();
+d1.show();
+}
+}
+
