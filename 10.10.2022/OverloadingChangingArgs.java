b5a05df1974cbe562aced0856c561b6f9a7fa025
shifa7699 <111556272+shifa7699@users.noreply.github.com>
2022-10-10 11:56:17 +0530

    Add files via upload

---
+++ 10.10.2022/OverloadingChangingArgs.java
@@ -0,0 +1,13 @@
+//Method Overloading - same name method with
+public class OverloadingChangingArgs {
+	static int sum(int a, int b) { //Two arguments
+		return a+b; }
+	static int sum(int a, int b, int c) { //Three arguments
+		return a+b+c;
+	}	
+	public static void main(String[] args) {
+		System.out.println(OverloadingChangingArgs.sum(6, 8));
+		System.out.println(OverloadingChangingArgs.sum(5,7,9));
+	}
+
+}
