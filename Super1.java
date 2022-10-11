73a7beea0bbda2d17dd31faa8b4861272062a865
shifa7699 <111556272+shifa7699@users.noreply.github.com>
2022-10-11 12:42:53 +0530

    Add files via upload

---
+++ Super1.java
@@ -0,0 +1,23 @@
+//super keyword --->Reference variable which refers immediate parent class
+
+class Shape // Parent class
+{
+	String name="Circle"; //data member of parent class
+}
+class Size extends Shape //Child class
+{
+	String name="Triangle";// Data member of child class
+	void print() // Child class method
+	{
+		System.out.println("Print Triangle");
+		System.out.println(super.name); //super.args
+	}
+}
+public class Super1 
+{
+	public static void main(String args[])
+	{
+		Size s=new Size();
+		s.print();
+	}
+}
