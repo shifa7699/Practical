784cc8aba7956543b894c4f242d6d214018d7d68
shifa7699 <111556272+shifa7699@users.noreply.github.com>
2022-10-12 13:54:45 +0530

    Add files via upload

---
+++ TestBinding.java
@@ -0,0 +1,35 @@
+
+/*class Binding 
+{
+	private void display() {
+			System.out.println("Samjhe???");
+		}
+		void show()
+		{
+			System.out.println("Ab samjhe???");
+		}
+	class Binding
+		public static void main(String args[])
+		{
+			Binding obj = new Binding();
+			obj.display();
+		}
+}*/
+class TestBinding
+{
+void show()
+{
+System.out.println("ab samjhe??");
+}}
+class Binding extends TestBinding
+{
+	void show()
+	{
+		System.out.println("Override");
+	}
+	public static void main(String args[])
+	{
+		TestBinding obj=new TestBinding();
+		obj.show();
+	}
+}
