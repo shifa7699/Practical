410f016cf1f222a0139f11aa4ea699ef02fb205c
Shifa Iqbal <111556272+shifa7699@users.noreply.github.com>
2022-10-31 13:28:37 +0530

    Update AnonymousArray.java

--- AnonymousArray.java
+++ AnonymousArray.java
@@ -4,7 +4,7 @@ public class AnonymousArray
 {
 	static void print(int arr[])
 	{
-		for(int i=1;i<arr.length;i++)
+		for(int i=0;i<arr.length;i++)
 			System.out.println(arr[i]);
 	}
 	public static void main(String args[])
