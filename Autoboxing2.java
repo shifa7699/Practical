028f5ba9ab14b15048cfb2155da2b05a3f6b82f7
Shifa Iqbal <111556272+shifa7699@users.noreply.github.com>
2022-11-07 11:45:55 +0530

    Add files via upload

---
+++ Autoboxing2.java
@@ -0,0 +1,15 @@
+package pack.com;
+
+import java.util.ArrayList;
+
+public class Autoboxing2 
+{
+	public static void main(String[] args) {
+		ArrayList<Integer>a1=new ArrayList<Integer>();
+		//Creating an empty array list of primitive type
+		a1.add(1); //adding the int primitive type value by using add()
+		a1.add(2);
+		a1.add(3);
+		System.out.println("ArrayList: "+a1);
+	}
+}
