1417ff0bd2c106e1cee51ee93f4a8fb67edb267a
Shifa Iqbal <111556272+shifa7699@users.noreply.github.com>
2022-11-07 11:51:53 +0530

    Add files via upload

---
+++ Unboxing.java
@@ -0,0 +1,11 @@
+package pack.com;
+//Wrapper to primitive
+public class Unboxing 
+{
+	public static void main(String[] args) {
+		Integer a=new Integer(2);
+		Integer i=a.intValue(); //converting Integer to int type
+		Integer j=a;  //now compiler will write int.value()
+		System.out.println(a+" "+i+" "+j);
+	}
+}
