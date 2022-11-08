66c6e044c1bed669114fca277b2c61744ef56224
Shifa Iqbal <111556272+shifa7699@users.noreply.github.com>
2022-11-08 13:51:14 +0530

    Add files via upload

---
+++ StringLength.java
@@ -0,0 +1,15 @@
+package pack.com;
+
+public class StringLength 
+{
+	public static void main(String[] args) {
+		String str="Anudip Foundation";
+		
+		//Get the length of str
+		int len=str.length();
+		System.out.println("The length of the string "+str+" is: "+len);
+		
+		//System.out.println("The length of the String "+str+"is : "+str.length()); 
+		
+	}
+}
