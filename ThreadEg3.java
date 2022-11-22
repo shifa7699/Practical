8f56b94e978032d7847a295f540867d30566f2be
Shifa Iqbal <111556272+shifa7699@users.noreply.github.com>
2022-11-22 13:50:52 +0530

    Add files via upload

---
+++ ThreadEg3.java
@@ -0,0 +1,26 @@
+package thread;
+
+public class ThreadEg3 extends Thread
+{
+	
+	public void run()
+	{
+		for ( int i = 0; i < 5; i++)
+		{
+			try {
+				Thread.sleep(800); }
+			catch(InterruptedException e) {
+				System.out.println(e);
+			}
+			System.out.println(i);
+		}
+	}
+	public static void main(String[] args) {
+		ThreadEg3  t=new ThreadEg3();
+		ThreadEg3  t1=new ThreadEg3();
+		ThreadEg3  t2=new ThreadEg3();
+		t.start();
+		t1.run();
+		t2.run();
+	}
+}
